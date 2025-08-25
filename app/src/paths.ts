6b408036bc755c7fff5b13794a0fcecb07b6772d
Rodrigo  Santos <rodrigo.santos@upgraide.ai>
2025-08-25 15:45:19 +0100

    [#007] Add admin panel URL paths

--- app/src/paths.ts
+++ app/src/paths.ts
@@ -9,3 +9,12 @@ export const termsPath = () => "/terms";
 
 // Dashboard Paths
 export const dashboardPath = () => "/dashboard";
+
+// Admin Dashboard Paths
+export const adminDashboardPath = () => "/admin";
+
+// Admin Investments Part
+export const adminInvestmentsPath = () => "/admin/investments";
+export const adminInvestmentDetailsPath = (id: string) =>
+  `/admin/investments/${id}`;
+export const adminInvestmentCreatePath = () => "/admin/investments/create";
