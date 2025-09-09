c494f89177d41b05c5ea2b6f01d2cb758f50d59f
Rodrigo Santos <rodrigo.santos@upgraide.ai>
2025-09-09 17:18:56 +0100

    feat(harbor-app): add robots and sitemap generation functionality
    Introduced new files for robots and sitemap generation, enhancing SEO capabilities. The robots.ts file defines rules for web crawlers, while sitemap.ts generates a sitemap with the base URL, improving site indexing and visibility.

---
+++ harbor-app/apps/web/app/sitemap.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from "next";
+
+export const baseUrl = "https://www.harborpartners.pt/";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const routes = [""].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
+  return [...routes];
+}
