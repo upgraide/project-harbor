67ad87569ba14fd774ebd9ac1eca2696d29e18c7
Rodrigo Santos <rodrigo.santos@upgraide.ai>
2025-09-11 12:26:39 +0100

    feat(dashboard): expand configuration and add new layout components
    Updated the Next.js configuration to include additional packages for transpilation. Introduced new layout components for the dashboard, including a header, navigation, language switcher, and theme switcher, enhancing the user interface and overall functionality. Implemented user preloading in the layout to improve user experience.

--- harbor-app/apps/dashboard/next.config.ts
+++ harbor-app/apps/dashboard/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  transpilePackages: ["@harbor-app/ui"],
+  transpilePackages: [
+    "@harbor-app/ui",
+    "@harbor-app/location",
+    "@harbor-app/backend",
+  ],
 };
 
 export default nextConfig;
