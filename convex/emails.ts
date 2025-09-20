c3e32949e7683f456addf7c2651feee4183ed262
Rodrigo Santos <rodrigo.santos@upgraide.ai>
2025-09-20 19:39:11 +0100

    feat: update email sending logic and configuration in emails.ts
    Modified the email sending functionality by updating the sender and recipient details for the test email. Additionally, configured the Resend instance to disable test mode, ensuring that emails are sent in a production-ready state.

--- convex/emails.ts
+++ convex/emails.ts
@@ -2,13 +2,15 @@ import { components } from "./_generated/api";
 import { Resend } from "@convex-dev/resend";
 import { internalMutation } from "./_generated/server";
 
-export const resend: Resend = new Resend(components.resend, {});
+export const resend: Resend = new Resend(components.resend, {
+  testMode: false,
+});
 
 export const sendTestEmail = internalMutation({
   handler: async (ctx) => {
     await resend.sendEmail(ctx, {
-      from: "Me <test@mydomain.com>",
-      to: "delivered@resend.dev",
+      from: "Acme <onboarding@resend.dev>",
+      to: "rodrigorafaelsantos7@icloud.com",
       subject: "Hi there",
       html: "This is a test email",
     });
