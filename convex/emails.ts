dfb58fa26d8b30c042508af98b665e9352cde9a8
Rodrigo Santos <rodrigo.santos@upgraide.ai>
2025-09-21 09:33:56 +0100

    feat: add reset password email functionality with localization support
    Implemented a new action to send reset password emails, incorporating localization for English and Portuguese. Enhanced the email subject lines based on user locale and integrated the ResetPasswordEmailEn component for rendering the email content. This addition improves user experience by providing localized communication during the password reset process.

--- convex/emails.ts
+++ convex/emails.ts
@@ -6,6 +6,7 @@ import { render, pretty } from "@react-email/render";
 import { InviteUserEmailEn } from "@/emails/invite-user-en";
 import React from "react";
 import { InviteUserEmailPt } from "@/emails/invite-user-pt";
+import { ResetPasswordEmailEn } from "@/emails/reset-password-en";
 
 export const resend: Resend = new Resend(components.resend, {
   testMode: false,
@@ -52,7 +53,41 @@ export const sendInviteUserEmail = action({
       from: "Acme <onboarding@resend.dev>",
       to: "rodrigorafaelsantos7@icloud.com",
       subject:
-        "Convite Exclusivo - Acesso à Harbor Exclusive Investment Opportunities",
+        args.locale === "en"
+          ? "Exclusive Invitation - Access to Harbor Exclusive Investment Opportunities"
+          : "Convite Exclusivo - Acesso à Harbor Exclusive Investment Opportunities",
+      html: html,
+    });
+
+    return null;
+  },
+});
+
+export const sendResetPasswordEmail = action({
+  args: {
+    toName: v.string(),
+    toEmail: v.string(),
+    resetPasswordLink: v.string(),
+    locale: v.string(),
+  },
+  returns: v.null(),
+  handler: async (ctx, args) => {
+    const html = await pretty(
+      await render(
+        React.createElement(ResetPasswordEmailEn, {
+          toName: args.toName,
+          resetPasswordLink: args.resetPasswordLink,
+        }),
+      ),
+    );
+
+    await resend.sendEmail(ctx, {
+      from: "Acme <onboarding@resend.dev>",
+      to: "rodrigorafaelsantos7@icloud.com",
+      subject:
+        args.locale === "en"
+          ? "Password Reset - Harbor Exclusive Investment Opportunities"
+          : "Redefinição de palavra-passe - Harbor Exclusive Investment Opportunities",
       html: html,
     });
 
