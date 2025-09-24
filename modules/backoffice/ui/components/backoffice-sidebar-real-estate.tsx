5d1f4c41f07b3f101343cd38b681ba31e99c886d
Rodrigo Santos <rodrigo.santos@upgraide.ai>
2025-09-24 15:35:23 +0100

    style: update sidebar component backgrounds for improved UI consistency
    Changed the background color of the sidebar menu items in the Mergers and Acquisitions and Real Estate components from 'bg-sidebar-primary' to 'bg-sidebar-foreground'. This update enhances the visual consistency and aligns with the overall theming of the application.

--- modules/backoffice/ui/components/backoffice-sidebar-real-estate.tsx
+++ modules/backoffice/ui/components/backoffice-sidebar-real-estate.tsx
@@ -122,7 +122,7 @@ export const BackofficeSidebarRealEstate = ({
             <SidebarMenuItem>
               <SidebarMenuButton size="lg" asChild className="md:h-8 md:p-0">
                 <Link href={backofficePath()}>
-                  <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <div className="bg-sidebar-foreground text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
                     <DynamicImage
                       className="size-4"
                       darkSrc={DarkIcon}
