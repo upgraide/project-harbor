99fc223cc05161aac9726a0a92374e1e93db8f69
Rodrigo Santos <rodrigo.santos@upgraide.ai>
2025-10-14 07:41:40 +0100

    feat: enhance styling and add user data fetching in backoffice
    Updated global CSS to improve shadow styles for better visual consistency. Modified the backoffice page to fetch and display user data securely using TRPC, ensuring only authenticated users can access this information. Introduced a new protected procedure in TRPC for user data retrieval, enhancing the application's security and data handling capabilities.

--- src/app/[locale]/(protected)/backoffice/page.tsx
+++ src/app/[locale]/(protected)/backoffice/page.tsx
@@ -1,11 +1,15 @@
 import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
 const Page = async () => {
   await requireAuth();
 
+  const data = await caller.getUsers();
+
   return (
-    <div className="flex min-h-screen min-w-screen items-center justify-center">
+    <div className="flex min-h-screen min-w-screen flex-col items-center justify-center gap-6">
       protected server component
+      <div>{JSON.stringify(data)}</div>
     </div>
   );
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,15 @@
 import prisma from "@/lib/db";
-import { baseProcedure, createTRPCRouter } from "../init";
+import { createTRPCRouter, protectedProcedure } from "../init";
 
 export const appRouter = createTRPCRouter({
-  getUsers: baseProcedure.query(() => prisma.user.findMany()),
+  getUsers: protectedProcedure.query(({ ctx }) => {
+    prisma.user.findMany({
+      where: {
+        id: ctx.auth.user.id,
+      },
+    });
+  }),
 });
+
 // export type definition of API
 export type AppRouter = typeof appRouter;
