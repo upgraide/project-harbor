1b88940dbe276f85dededfeb9f05700233f82652
Diogo Babo <diogo.babo@gmail.com>
2025-12-09 00:29:39 +0100

    feat: Add CORS headers for site.webmanifest and update site URLs to new deployment

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,27 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/site.webmanifest",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withSentryConfig(nextConfig, {
